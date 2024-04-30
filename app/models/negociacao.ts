3075ec643cfec81a9297afb312b217a01b622c20
guilhermehassacoopers <gui.hassa@hotmail.com>
2024-04-29 21:21:05 -0300

    🚀 Projeto da Aula

---
+++ app/models/negociacao.ts
@@ -0,0 +1,19 @@
+export class Negociacao {
+  
+
+  constructor(
+    private _data: Date,
+    public quantidade: number,
+    public valor: number
+
+  ) {}
+
+  get volume():number {
+    return this.quantidade * this.valor;
+  }
+
+  get data(): Date{
+    const data = new Date(this._data.getTime())
+    return data;
+  }
+}
